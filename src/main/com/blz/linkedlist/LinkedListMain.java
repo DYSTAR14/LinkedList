706ec3c3e4b369dc2163e1c42461f4aee2400263
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-29 12:59:00 +0530

    Add: Element add at First Position

--- src/main/com/blz/linkedlist/LinkedListMain.java
+++ src/main/com/blz/linkedlist/LinkedListMain.java
@@ -7,9 +7,27 @@ public class LinkedListMain {
 	static final Scanner SC = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Single LinkedList World");
-		list.addFirst(list, 56);
-		list.addFirst(list, 30);
-		list.addFirst(list, 70);
-		list.display(list);
+		System.out.println("1.Add First\n2.Add Last\n3.Add Nth Position\n"
+				+ "4.Delete First\n5.Delete Last\n6.Delete Nth Position\n" + "7.Search\n8.Display LinkedList\n0.Exit");
+		int exit = 1;
+		while (exit != 0) {
+			System.out.println("Select From Option");
+			int option = SC.nextInt();
+			switch (option) {
+			case 0:
+				exit = 0;
+				System.out.println("Thank you For\nParticipation LinkedList.");
+				break;
+			case 1:
+				System.out.println("Enter Value");
+				list.addFirst(list, SC.nextInt());
+				break;
+			case 8:
+				list.display(list);
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
